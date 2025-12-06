617e3bd89e28018f9cd7777654b0696c74ee9efc
hj88nyyy <hjleeno1kr@gmail.com>
2025-12-06 17:15:46 +0900

    next.config.ts update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // 빌드 중에 ESLint 오류를 무시함
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // 빌드 중에 TypeScript 타입 오류를 무시함 (선택 사항)
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
